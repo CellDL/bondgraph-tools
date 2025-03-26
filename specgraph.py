d5a5d01885e804f79dead87bcab04f9b3d5d6e9e
David Brooks <d.brooks@auckland.ac.nz>
2025-03-26 22:54:41 +1300

    Extend our test runner...

--- specgraph.py
+++ specgraph.py
@@ -1,8 +1,23 @@
 from graph2celldl import Graph2CellDL
 
+#from bondgraph.model import load_model
+#from bondgraph.template import BondgraphTemplate
+
 from bondgraph.spec2networkx import bg_spec_to_networkx
+#from bondgraph.template import BondgraphTemplate
 
 
-G = bg_spec_to_networkx('/Users/dbro078/CellDL/Bondgraphs/ApiNATOMY/models/stomach-spleen.ttl')
-celldl = Graph2CellDL(G)
-celldl.save_diagram('stomach.svg')
+#template = '/Users/dbro078/CellDL/Bondgraphs/ApiNATOMY/templates/vascular-segment-template.ttl'
+#tpl = BondgraphTemplate(template)
+
+#print()
+
+specification = '/Users/dbro078/CellDL/Bondgraphs/ApiNATOMY/models/stomach-spleen.ttl'
+#model = load_model(specification)
+
+
+G = bg_spec_to_networkx(specification)
+## #print(G.nodes(data=True))
+## print(G.edges())
+## celldl = Graph2CellDL(G)
+## celldl.save_diagram('stomach.svg')
