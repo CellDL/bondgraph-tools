a86645691f394cc19bc717d2943fe21e3dea9e8b
David Brooks <d.brooks@auckland.ac.nz>
2025-04-01 22:14:57 +1300

    Update our development runner.

--- specgraph.py
+++ specgraph.py
@@ -1,23 +1,31 @@
 from graph2celldl import Graph2CellDL
 
-#from bondgraph.model import load_model
-#from bondgraph.template import BondgraphTemplate
-
-from bondgraph.spec2networkx import bg_spec_to_networkx
-#from bondgraph.template import BondgraphTemplate
+from bondgraph import load_model
+from bondgraph.cellml import CellMLModel
+from bondgraph.template import TemplateRegistry
 
 
-#template = '/Users/dbro078/CellDL/Bondgraphs/ApiNATOMY/templates/vascular-segment-template.ttl'
-#tpl = BondgraphTemplate(template)
+template = './data/vascular-segment-template.ttl'
+registry = TemplateRegistry(template)
 
-#print()
+specification = 'data/single-segment.ttl'
+output = 'single-segment'
 
-specification = '/Users/dbro078/CellDL/Bondgraphs/ApiNATOMY/models/stomach-spleen.ttl'
-#model = load_model(specification)
+#specification = './data/stomach-spleen.ttl'
+#output = 'stomach-spleen'
 
 
-G = bg_spec_to_networkx(specification)
-## #print(G.nodes(data=True))
-## print(G.edges())
-## celldl = Graph2CellDL(G)
-## celldl.save_diagram('stomach.svg')
+model = load_model(specification, registry)
+if model is not None:
+    if model.disconnected:
+        raise ValueError('Model is not a connected bondgraph...')
+
+    G = model.nx_graph()
+    celldl = Graph2CellDL(G)
+    celldl.save_diagram(f'{output}.svg')
+
+    cellml = CellMLModel(model.name)
+    for node in model.nodes:
+        cellml.add_node(node)
+    with open(f'{output}.cellml', 'wb') as fp:
+        fp.write(cellml.to_xml())
