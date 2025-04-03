61c6d9ad69de706c571878aedab6745bcdc621f2
David Brooks <d.brooks@auckland.ac.nz>
2025-04-03 17:53:42 +1300

    Add command line parameter handling to the main application.

--- specgraph.py
+++ specgraph.py
@@ -1,31 +1,65 @@
+#===============================================================================
+#
+#  CellDL and bondgraph tools
+#
+#  Copyright (c) 2020 - 2025 David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===============================================================================
+
 from celldltools.graph2celldl import Graph2CellDL
 
-from bondgraph.bondgraph import load_model
+from bondgraph.bondgraph import load_model, __version__
 from bondgraph.bondgraph.cellml import CellMLModel
 from bondgraph.bondgraph.template import TemplateRegistry
 
+#===============================================================================
 
-template = './data/vascular-segment-template.ttl'
-registry = TemplateRegistry(template)
+def main():
+#==========
+    import argparse
 
-specification = 'data/single-segment.ttl'
-output = 'single-segment'
+    parser = argparse.ArgumentParser(description='Generate CellML for a bondgraph model specified in RDF')
+    parser.add_argument('--version', action='version', version=f'Version {__version__}')
+    parser.add_argument('--celldl', metavar='CELLDL_FILE', help='The name of the resulting CellDL (SVG) output file. Optional')
+    parser.add_argument('template', metavar='TEMPLATE_FILE', help='A template file defining bondgraph components in RDF')
+    parser.add_argument('model', metavar='MODEL_FILE', help='The RDF definition of a model')
+    parser.add_argument('cellml', metavar='CELLML_FILE', help='The name of the resulting CellML file')
+    args = parser.parse_args()
 
-#specification = './data/stomach-spleen.ttl'
-#output = 'stomach-spleen'
-
-
-model = load_model(specification, registry)
-if model is not None:
-    if model.disconnected:
+    registry = TemplateRegistry(args.template)
+    model = load_model(args.model, registry)
+    if model is None:
+        raise TypeError('The model could not be loaded')
+    elif model.disconnected:
         raise ValueError('Model is not a connected bondgraph...')
 
-    G = model.nx_graph()
-    celldl = Graph2CellDL(G)
-    celldl.save_diagram(f'{output}.svg')
+    if args.celldl:
+        G = model.nx_graph()
+        celldl = Graph2CellDL(G)
+        celldl.save_diagram(args.celldl)
 
     cellml = CellMLModel(model.name)
     for node in model.nodes:
         cellml.add_node(node)
-    with open(f'{output}.cellml', 'wb') as fp:
+    with open(args.cellml, 'wb') as fp:
         fp.write(cellml.to_xml())
+
+#===============================================================================
+
+if __name__ == '__main__':
+#=========================
+    main()
+
+#===============================================================================
